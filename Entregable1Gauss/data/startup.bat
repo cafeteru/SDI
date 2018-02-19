85948ec866a8e172fe9102df4b9170f406a63d94
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2018-02-19 01:39:57 +0100

    Comporbacion de datos con Jquery para ejercicio 01

---
+++ Entregable1Gauss/data/startup.bat
@@ -0,0 +1,1 @@
+@java -classpath lib/hsqldb.jar org.hsqldb.server.Server %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ sdi1-UO239795/src/main/java/com/uniovi/entities/User.java
@@ -0,0 +1,96 @@
+package com.uniovi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+@Entity
+@Table(name = "TUSERS")
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(unique = true)
+	@NotNull
+	private String email;
+
+	private String name;
+
+	@NotNull
+	private String password;
+
+	public User() {
+	}
+
+	public User(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		setPassword(password);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = DigestUtils.md5Hex(password);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [email=" + email + ", name=" + name + ", password="
+				+ password + "]";
+	}
+
+}

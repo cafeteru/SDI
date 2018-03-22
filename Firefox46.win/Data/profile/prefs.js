178f37a5a4d89e910a9fddd8c9497f7b17f1d5d2
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2018-03-22 15:38:18 +0100

    Clase 22/03/18

--- Firefox46.win/Data/profile/prefs.js
+++ Firefox46.win/Data/profile/prefs.js
@@ -428,3 +428,5 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
 
 user_pref("browser.shell.checkDefaultBrowser", false);
+
+user_pref("browser.shell.checkDefaultBrowser", false);
---
+++ Tienda_NodeJs_1/main.js
@@ -0,0 +1,22 @@
+// Módulos
+var express = require('express');
+var app = express();
+
+var swig = require('swig');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public')); // Funcionalidad adicional a la aplicación
+
+//Variables
+app.set("port",8081);
+
+require("./routes/rusuarios.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig);
+
+// lanzar el servidor
+app.listen(app.get("port"), function () {
+    console.log("Servidor activo");
+});

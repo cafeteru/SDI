cd41d07426c5f238e6b353898ab28464afd03ec6
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2018-05-05 02:02:05 +0200

    jQuery ejercicio 7 acabados

--- sdi2-UO239795/routes/api.js
+++ sdi2-UO239795/routes/api.js
@@ -54,7 +54,7 @@ module.exports = function (app, repository, ObjectId) {
                     sender: res.user,
                     receiver: req.body.email,
                     message: req.body.message,
-                    date: timeStamp(),
+                    date: new Date(),
                     read: false
                 }
                 repository.addElement(message, "messages", function (id) {
@@ -102,12 +102,8 @@ module.exports = function (app, repository, ObjectId) {
         });
     });
 
-    app.get("/api/messages/unread", function (req, res) {
-        let messages = {
-            receiver: res.user,
-            read: false
-        };
-        repository.getElements(messages, "messages", function (conversation) {
+    app.get("/api/messages/all", function (req, res) {
+        repository.getElements({}, "messages", function (conversation) {
             res.status(200);
             res.send(JSON.stringify(conversation));
         });
@@ -185,34 +181,5 @@ module.exports = function (app, repository, ObjectId) {
                 });
             }
         });
-    }
-
-    function timeStamp() {
-        // Create a date object with the current time
-        var now = new Date();
-
-        // Create an array with the current month, day and time
-        var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
-
-        // Create an array with the current hour, minute and second
-        var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
-
-        // Determine AM or PM suffix based on the hour
-        var suffix = (time[0] < 12) ? "AM" : "PM";
-
-        // Convert hour from military time
-        time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
-
-        // If hour is 0, set it to 12
-        time[0] = time[0] || 12;
-
-        // If seconds and minutes are less than 10, add a zero
-        for (var i = 1; i < 3; i++) {
-            if (time[i] < 10) {
-                time[i] = "0" + time[i];
-            }
-        }
-        // Return the formatted string
-        return date.join("/") + " " + time.join(":") + " " + suffix;
     }
 };

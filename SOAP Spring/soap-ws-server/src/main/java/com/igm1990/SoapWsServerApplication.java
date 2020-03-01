fa0847a36273b1fdb4819cfc584c4526765ff9bb
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2020-03-01 18:29:18 +0100

    Ejemplo de servidor SOAP

---
+++ SOAP Spring/soap-ws-server/src/main/java/com/igm1990/SoapWsServerApplication.java
@@ -0,0 +1,13 @@
+package com.igm1990;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWsServerApplication.class, args);
+	}
+
+}
---
+++ SOAP Spring/soap-ws-server/src/test/java/com/igm1990/SoapWsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igm1990;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
